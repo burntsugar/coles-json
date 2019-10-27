7c8c4edbd526630196fbdd335a781554db9e39d2
burntsugar <rrakky89@gmail.com>
2019-10-27 13:42:24 +1100

    Fin

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Coles Data 
 
 ![](images/chrome-console-coles-list.png)
+
+I wanted some product data to work with in tutorials so I dived into the source of my Coles page.
--- coles-json.rb
+++ coles-json.rb
@@ -38,3 +38,61 @@ def print_stock_list
 
 end
 
+#
+# Returns a 2D array containing product names at 0 and prices at 1.
+#
+def stock_list_to_a
+
+    arr = [[],[]]
+
+    json_from_file = File.read('stock-list.json')
+    data_hash = JSON.parse(json_from_file)
+
+    data_hash["products"].each_with_index { |i,index|
+        product = i["m"] + " " + i["n"] + " " + i["a"]["O3"][0]
+        price = i["p1"]["o"]
+        arr[0] << product
+        arr[1] << price
+    }
+
+    arr
+
+end
+
+def print_stock_list_array(arr)
+
+    arr[0].each_with_index { |item,index|
+        puts "#{item} #{"%0.2f" % arr[1][index]}"
+    }
+
+end
+
+#
+# Returns a string which can be pasted into a simple product names array.
+#
+def print_prod_names_as_array_vals(arr)
+
+    s = ""
+    arr[0].each { |p|
+        s += '"' + p + '"' + ","
+    }
+    s.chomp!(",")
+    
+end
+
+#
+# Returns a string which can be pasted into a simple product prices array.
+#
+def print_prod_prices_as_array_vals(arr)
+    s = ""
+    arr[1].each { |p|
+        s += "#{p.to_s},"
+    }
+    s.chomp!(",")
+    puts s
+end
+
+#  print_stock_list_array(stock_list_to_a)
+#  print_prod_names_as_array_vals(stock_list_to_a)
+#  print_prod_prices_as_array_vals(stock_list_to_a)
+
